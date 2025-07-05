3f106a0a54d66ba2601639e9ad101fc87217ca0f
GYORGY03 <hernandezjorgeluis738@gmail.com>
2025-07-05 14:17:59 -0400

    fix type

--- src/features/review/types.ts
+++ src/features/review/types.ts
@@ -1,10 +1,10 @@
 import { Schema } from 'mongoose';
-import { BaseIdentity } from '../../types/general';
+import { BaseIdentity, Image } from '../../types/general';
 
 export interface Review extends BaseIdentity {
   reviewerId: Schema.Types.ObjectId;
   postId: Schema.Types.ObjectId;
   star: number;
   comment: string;
-  images?: string[];
+  images?: Image[];
 }
